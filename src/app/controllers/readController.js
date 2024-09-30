7b194398ca4e50a83341a96c5487dddaa67b270c
NguyenNam <hainam13122004@gmail.com>
2024-09-30 23:48:54 +0700

    thay mongoose-slug-generator thành slugify

--- src/app/controllers/readController.js
+++ src/app/controllers/readController.js
@@ -18,30 +18,38 @@ class ReadController {
   }
 
     // [POST] /films/store
-  store(req, res, next) {
-      const formData = req.body; 
+  async store(req, res, next) {
+    try {
+      const formData = req.body;
       const film = new Film(formData);
-      film.save()
-          .then(() => res.redirect('/films'))
-          .catch(error => {
-              console.error('Lỗi khi lưu phim:', error);
-              res.status(500).send('Có lỗi xảy ra trong quá trình lưu phim.');
-          });
+      await film.save();  // Sử dụng await để lưu dữ liệu
+      res.redirect('/films');
+    } catch (error) {
+      console.error('Lỗi khi lưu phim:', error);
+      res.status(500).send('Có lỗi xảy ra trong quá trình lưu phim.');
+    }
   }
+    
 
     // [GET] /films/:id/edit
-  edit(req, res, next) {
-    Film.findById(req.params.id)
-      .then(film => res.render('films/edit', { film: mongooseToObject(film) }))
-      .catch(next); 
+  async edit(req, res, next) {
+    try {
+      const film = await Film.findById(req.params.id);  // Sử dụng await
+      res.render('films/edit', { film: mongooseToObject(film) });
+    } catch (error) {
+      next(error);
+    }
   }
+    
 
     // [PUT] /films/:id
-  update(req, res, next) {
-    Film.updateOne({ _id: req.params.id }, req.body)
-      .then(() => res.redirect('/me/stored/films'))
-      .catch(next);
+  async update(req, res, next) {
+    try {
+      await Film.updateOne({ _id: req.params.id }, req.body);  
+      res.redirect('/me/stored/films');
+    } catch (error) {
+      next(error);  
+    }
   }
 }
-
 export default new ReadController();
--- src/app/models/Film.js
+++ src/app/models/Film.js
@@ -1,25 +1,29 @@
 import mongoose from 'mongoose';
-import slug from 'mongoose-slug-generator';
+import slugify from 'slugify';
 
-mongoose.plugin(slug);
-
-const Schema = mongoose.Schema;
-
-const FilmSchema = new Schema({
+const FilmSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    note: { type: String }, 
+    note: { type: String },
     dao_dien: { type: String },
-    dien_vien: { type: String }, 
+    dien_vien: { type: String },
     the_loai: { type: String },
     khoi_chieu: { type: Date },
-    thoi_luong: { type: String }, 
-    ngon_ngu: { type: String }, 
-    slug: { type: String, slug: "title", unique: true },
-    src: { type: String }, 
+    thoi_luong: { type: String },
+    ngon_ngu: { type: String },
+    slug: { type: String, unique: true },
+    src: { type: String },
 }, {
     timestamps: true,
+});
+
+FilmSchema.pre('save', function(next) {
+    if (this.title && !this.slug) {
+        this.slug = slugify(this.title, { lower: true, strict: true });
+    }
+    next();
 });
 
 const Film = mongoose.model('Film', FilmSchema);
 
 export default Film;
+
